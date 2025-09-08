486e9bcc8fd883a9716be325dcfce081f105f745
Ítalo H. <italodssouza.backup@gmail.com>
2025-09-08 10:55:00 -0300

    client-server: creating the client server

---
+++ client-server/src/test/java/tech/kvothe/client_server/ClientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kvothe.client_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
